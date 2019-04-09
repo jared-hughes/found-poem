beca39d2879670b6ad454d8c98b06c21d639f6ff
Jared Hughes <jahughes241@gmail.com>
2019-04-08 22:29:56 -0700

    Slightly prettier

--- src/data.js
+++ src/data.js
@@ -1,9 +1,25 @@
 let data = {
   originalSource: {
     text: `
-      Outside the door I am aware of the darkness
+      Outside the door I am aware of the darkness and the wind as a deliverance.
 
-      and the wind as a deliverance. I breathe as deep as I can, and feel the breeze in my face, warm and soft as never before. Thoughts of girls, of flowery meadows, of white clouds suddenly come into my head. My feet begin to move forward in my boots, I go quicker, I run. Soldiers pass by me, I hear their voices without understanding. The earth is streaming with forces which pour into me through the soles of my feet. The night crackles electrically, the front thunders like a concert of drums. My limbs move supplely, I feel my joints strong, I breathe the air deeply. The night lives, I live. I feel a hunger, greater than comes from the belly alone.
+      I breathe as deep as I can, and feel the breeze in my face, warm and soft as never before.
+
+      Thoughts of girls, of flowery meadows, of white clouds suddenly come into my head.
+
+      My feet begin to move forward in my boots, I go quicker, I run.
+
+      Soldiers pass by me, I hear their voices without understanding.
+
+      The earth is streaming with forces which pour into me through the soles of my feet.
+
+      The night crackles electrically, the front thunders like a concert of drums.
+
+      My limbs move supplely, I feel my joints strong, I breathe the air deeply.
+
+      The night lives, I live.
+      
+      I feel a hunger, greater than comes from the belly alone.
     `,
     background: "images/helmet.jpg",
     light: true,
