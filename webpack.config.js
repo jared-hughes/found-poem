d6ac552099ea0fa93a1d7d0288f2664b0f94f917
Jared Hughes <jahughes241@gmail.com>
2019-04-08 21:03:01 -0700

    Init environment

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              "@babel/preset-env",
+              "@babel/preset-react"
+            ]
+          }
+        }
+      },
+      {
+        test: /\.json5$/,
+        use: {
+          loader: "json5-loader",
+        }
+      }
+    ]
+  }
+}
